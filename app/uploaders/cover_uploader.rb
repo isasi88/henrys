d4af26088d94f03207c8234cc81a992db16154c4
isasi88 <kikeisasi@gmail.com>
2017-04-21 21:00:22 +0200

    Fix

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -37,11 +37,11 @@ class CoverUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_fill => [300, 300]
+     process :resize_to_fit => [300, 300]
   end
 
   version :big do
-     process :resize_to_fill => [600, 600]
+     process :resize_to_fit => [600, 600]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
